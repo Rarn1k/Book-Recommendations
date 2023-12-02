3f3bb4a70226f2a66b66dfdb4df0b3b3814a7853
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-12-02 10:32:11 +0300

    add carousel of genres

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, genre_books
 
 urlpatterns = [
     path('', index, name='index'),
+    path("genre_books/<genre>", genre_books, name="genre_books"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def index(request):
     return render(request, 'mainapp/index.html')
+
+
+def genre_books(request, genre):
+    return HttpResponseRedirect('/')
