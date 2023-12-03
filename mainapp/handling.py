08030a0956b2a5f22e8241edb6b75c0a0dfb97df
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-12-03 18:08:15 +0300

    popular

--- mainapp/handling.py
+++ mainapp/handling.py
@@ -1,7 +1,9 @@
+import random
+import operator
 import pandas as pd
 
 
-def get_top_books_genre(books):
+def get_top_books(books):
     books_data = list(books.values())
     dfbook = pd.DataFrame.from_records(books_data)
 
@@ -13,5 +15,25 @@ def get_top_books_genre(books):
     dfbook = dfbook.assign(weighted_rating=W)
     dfbook.sort_values("weighted_rating", ascending=False, inplace=True)
 
-    books_res = dfbook['title'].head(48).sample(16).values
-    return books_res
+    return dfbook
+
+
+def get_popular_among_users(all_ratings, books, n):
+    random.shuffle(all_ratings)
+    best_users_ratings = sorted(all_ratings, key=operator.attrgetter("book_rating"), reverse=True)
+    filtered_books = set()
+    for i, rating in enumerate(best_users_ratings):
+        if rating.book_rating >= 4:
+            filtered_books.add(rating.book.id)
+        elif rating.book_rating < 4 or len(filtered_books) == n:
+            break
+
+    remaining_books = n - len(filtered_books)
+    if remaining_books >= 0:
+        rem_books = get_top_books(books)['id'].head(2 * n).sample(2 * n).tolist()
+        filtered_books = (
+                list(filtered_books)
+                + list(set(rem_books) - filtered_books)[:remaining_books]
+        )
+    return filtered_books
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from .models import Book, Genre
-from .handling import get_top_books_genre
+from .models import Book, Genre, UserRating
+from .handling import get_top_books, get_popular_among_users
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    all_ratings = list(UserRating.objects.all().order_by("-book_rating"))
+    books = Book.objects.all()
+    popular_books_id = get_popular_among_users(all_ratings, books, 15)
+    popular_books = Book.objects.filter(id__in=popular_books_id)
+    context = {
+        'books': popular_books
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def genre_books(request, genre):
 
     books = Genre.objects.get(name=genre.lower()).book.all()
-    books_res = get_top_books_genre(books)
+    books_res = get_top_books(books)
+    books_res = books_res['title'].head(48).sample(16).values
     books_response = Book.objects.filter(title__in=books_res)
     context = {
         "genre": genre.capitalize(),
