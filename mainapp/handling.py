a4c7e05d36222e2a97c63184b19f490e1187a4bf
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-12-05 17:37:17 +0300

    change popular books among users

--- mainapp/handling.py
+++ mainapp/handling.py
@@ -1,6 +1,4 @@
 import os
-import random
-import operator
 import pandas as pd
 import pickle
 import root.settings as settings
@@ -21,24 +19,11 @@ def get_top_books(books):
     return dfbook
 
 
-def get_popular_among_users(all_ratings, books, n):
-    random.shuffle(all_ratings)
-    best_users_ratings = sorted(all_ratings, key=operator.attrgetter("book_rating"), reverse=True)
-    filtered_books = set()
-    # for i, rating in enumerate(best_users_ratings):
-    #     if rating.book_rating >= 4:
-    #         filtered_books.add(rating.book.id)
-    #     elif rating.book_rating < 4 or len(filtered_books) == n:
-    #         break
+def get_popular_among_users(books, n):
 
-    remaining_books = n - len(filtered_books)
-    if remaining_books >= 0:
-        rem_books = get_top_books(books)['id'].head(2 * n).sample(2 * n).tolist()
-        filtered_books = (
-                list(filtered_books)
-                + list(set(rem_books) - filtered_books)[:remaining_books]
-        )
-    return filtered_books
+    books = get_top_books(books)['id'].head(2 * n).sample(n).tolist()
+
+    return books
 
 
 def get_predictions():
