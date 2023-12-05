0610a5348ec421a674cf4848a1040f6da7cbc8be
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-12-05 13:29:20 +0300

    temporarily comment

--- mainapp/handling.py
+++ mainapp/handling.py
@@ -22,11 +22,11 @@ def get_popular_among_users(all_ratings, books, n):
     random.shuffle(all_ratings)
     best_users_ratings = sorted(all_ratings, key=operator.attrgetter("book_rating"), reverse=True)
     filtered_books = set()
-    for i, rating in enumerate(best_users_ratings):
-        if rating.book_rating >= 4:
-            filtered_books.add(rating.book.id)
-        elif rating.book_rating < 4 or len(filtered_books) == n:
-            break
+    # for i, rating in enumerate(best_users_ratings):
+    #     if rating.book_rating >= 4:
+    #         filtered_books.add(rating.book.id)
+    #     elif rating.book_rating < 4 or len(filtered_books) == n:
+    #         break
 
     remaining_books = n - len(filtered_books)
     if remaining_books >= 0:
@@ -36,4 +36,6 @@ def get_popular_among_users(all_ratings, books, n):
                 + list(set(rem_books) - filtered_books)[:remaining_books]
         )
     return filtered_books
+
+
 
